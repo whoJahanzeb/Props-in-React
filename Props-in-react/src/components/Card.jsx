ca95bc361880b1a843d08241858faaa5c0de7427
Muhammad Jahanzeb <i.am.jahanzeb6@gmail.com>
2024-01-18 22:01:56 +0500

    Set name of class to className due to React in card component.

--- Props-in-react/src/components/Card.jsx
+++ Props-in-react/src/components/Card.jsx
@@ -1,17 +1,17 @@
 function Card({ src, title, description, profile }) {
   return (
     <>
-      <div class="relative h-[400px] w-[300px] rounded-md">
+      <div className="relative h-[400px] w-[300px] rounded-md">
         <img
           src={src}
           alt="AirMax Pro"
           class="z-0 h-full w-full rounded-md object-cover"
         />
-        <div class="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
-        <div class="absolute bottom-4 left-4 text-left">
-          <h1 class="text-lg font-semibold text-white">{title}</h1>
-          <p class="mt-2 text-sm text-gray-300">{description}</p>
-          <button class="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+        <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+        <div className="absolute bottom-4 left-4 text-left">
+          <h1 className="text-lg font-semibold text-white">{title}</h1>
+          <p className="mt-2 text-sm text-gray-300">{description}</p>
+          <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
             {profile}
           </button>
         </div>
