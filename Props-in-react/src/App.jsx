586197ca8a57b6b05292475fe59cd6a65f877539
Muhammad Jahanzeb <i.am.jahanzeb6@gmail.com>
2024-01-18 21:41:01 +0500

    Install tailwind in project, make component and reuse in different places.

--- Props-in-react/src/App.jsx
+++ Props-in-react/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="max-w-7xl mx-auto space-y-5">
+        <div className="text-3xl rounded-3xl bg-red-300 font-semibold max-w-lg mx-auto text-center">
+          Tailwind
+        </div>
+        <div className="grid grid-cols-4 gap-5">
+          <Card
+            src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+            title="Laura Johnson"
+            description="This is Laura from New York doing work as Software Engineer."
+            profile="View Profile →"
+          />
+          <Card
+            src="https://images.pexels.com/photos/17550988/pexels-photo-17550988/free-photo-of-man-walking-with-cellphone.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            title="Jahanzeb"
+            description="Muhammad Jahanzeb is learning React Js from youtube and doing work to learn React Js."
+            profile="View Profile →"
+          />
+          <Card
+            src="https://images.pexels.com/photos/160599/beauty-leather-style-girl-160599.jpeg?auto=compress&cs=tinysrgb&w=600"
+            title="Stephenie"
+            description="Hey, Stephenie here doing work as Laravel Developer."
+            profile="View Profile →"
+          />
+          <Card
+            src="https://images.pexels.com/photos/1321909/pexels-photo-1321909.jpeg?auto=compress&cs=tinysrgb&w=600"
+            title="Ling Ching"
+            description="Myself Ling Ching, working as game developer in software company in Pakistan. It's joke."
+            profile="View Profile →"
+          />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Props-in-react/src/components/Card.jsx
@@ -0,0 +1,22 @@
+function Card({ src, title, description, profile }) {
+  return (
+    <>
+      <div class="relative h-[400px] w-[300px] rounded-md">
+        <img
+          src={src}
+          alt="AirMax Pro"
+          class="z-0 h-full w-full rounded-md object-cover"
+        />
+        <div class="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+        <div class="absolute bottom-4 left-4 text-left">
+          <h1 class="text-lg font-semibold text-white">{title}</h1>
+          <p class="mt-2 text-sm text-gray-300">{description}</p>
+          <button class="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+            {profile}
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Card;
